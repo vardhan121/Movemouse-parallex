41c4c0cdff631341bbb84f944a7c6fbe05496a0a
vardhan121 <128150759+vardhan121@users.noreply.github.com>
2024-06-06 11:45:38 +0530

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,11 @@
+document.addEventListener("mousemove", parallax);
+
+function parallax(event) {
+    document.querySelectorAll(".mouse").forEach((shift) => {
+        const position = shift.getAttribute("value");
+        const x = (window.innerWidth - event.pageX * position) / 90;
+        const y = (window.innerHeight - event.pageY * position) / 90;
+
+        shift.style.transform = `translateX(${x}px) translateY(${y}px)`;
+    });
+}
